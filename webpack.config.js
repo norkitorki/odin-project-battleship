b6ef13d54abe1b9d2c4e71f67152c548f4bdc59c
norkitorki <norki@tuta.io>
2024-09-13 13:48:51 +0200

    Install webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  plugins: [new MiniCssExtractPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+};
