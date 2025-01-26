3ab42dffeddd14e6d4e072ad01c35093861f20be
norkitorki <norki@tuta.io>
2025-01-26 13:02:17 +0100

    Change webpack mode to production

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  mode: 'development',
+  mode: 'production',
   plugins: [new MiniCssExtractPlugin()],
   module: {
     rules: [
